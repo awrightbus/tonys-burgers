bf1709811199f94bc52d962677b5bc67ce4c580e
awright <awrightbus@gmail.com>
2020-10-12 23:16:10 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+        <h1>Tonys Burgers</h1>
+        {/* Header */}
+        {/* Hero */}
+        {/* HomePageCards */}
+        {/* Footer */}
+        {/* MobileNav */}
+    </div>
+  );
+}
+
+export default App;
